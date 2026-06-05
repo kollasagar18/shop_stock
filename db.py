a8a05930816cb750d5e4f84630eaedddbdded0e6
kollasagar18 <kollasagar03@gmail.com>
2026-06-05 17:51:33 +0530

    Added Railway MySQL environment variables

--- db.py
+++ db.py
@@ -3,9 +3,9 @@ import os
 
 def db_con():
     return mysql.connector.connect(
-        host=os.getenv("mysql.railway.internal"),
-        user=os.getenv("root"),
-        password=os.getenv("IQThGwvASQrDbFnqXkJsGWOzEfBBJXsH"),
-        database=os.getenv("railway"),
-        port=os.getenv("3306")
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=os.getenv("MYSQLPORT")
     )
